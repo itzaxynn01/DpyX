04e04f34ec54c6071eb3f3aec56dd5ad0021917d
itzaxynn01 <119340579+itzaxynn01@users.noreply.github.com>
2022-11-30 08:41:39 +0530

    Version 0.0.1
    First Commit

---
+++ lib/bot/__init__.py
@@ -0,0 +1,72 @@
+import discord
+from discord.ext.commands import Bot as BotBase
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from discord import Embed , File
+from datetime import datetime
+PREFIX = '+'
+OWNER_IDS = [1046816200237256734]
+
+class Bot(BotBase):
+    def __init__(self):
+        self.PREFIX = PREFIX
+        self.ready = False
+        self.guild = None
+        self.guild = None
+        self.scheduler = AsyncIOScheduler()
+
+        super().__init__(command_prefix=PREFIX  ,
+                            owner_ids = OWNER_IDS , 
+                        intents=discord.Intents.all(),)
+        
+    
+
+    def run(self , version):
+        self.VERSION = version
+
+
+        with open("./lib/bot/token.0", "r" , encoding="utf-8") as tf:
+            self.TOKEN = tf.read()
+
+        print("running bot ... ")
+        super().run(self.TOKEN , reconnect=True)
+
+
+    async def on_connect(self):
+        print("bot connected")
+
+    async def on_disconnect(self):
+        print("bot disconnected")
+
+    async def on_ready(self):
+        if not self.ready:
+            
+            self.ready = True
+            print("bot ready")
+
+            channel = self.get_channel(1046890155132330020)
+            await channel.send("now online")
+
+            embed = Embed(title="now online", description="DpyX is Now online",colour=0xFF0000,timestamp=datetime.utcnow())
+            fields =[("Name", "Value" , True),
+                    ("Another Field", "This field is next to oithe one" , True),
+                    ("a non inline field" , "this field will not appear on its own row",False)]
+        for name , value, inline in fields:
+                embed.add_field(name=name, value=value, inline=inline)
+                embed.set_footer(text="this is footer")
+                embed.set_author(name="dpyx",)
+                await channel.send(embed=embed)
+
+                await channel.send(file=File("./data/images/profile.png"))
+
+                
+
+        else:
+            print("bot reconnected")
+ 
+            
+
+    async def on_message(self, message):
+        pass
+
+
+bot = Bot()
